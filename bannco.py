8dd725183783620bd0ac136f6f5c4ace2017353c
André Lima <andrealplimapereira10@gmail.com>
2024-10-29 09:51:31 -0300

    criando funções de clientes

--- bannco.py
+++ bannco.py
@@ -16,3 +16,38 @@ def carregar_dados():
 def salvar_dados(clientes):
     with open(DATA_FILE, 'w') as file:
         json.dump(clientes, file)
+
+        # Função para criar um novo cliente
+def novo_cliente(clientes):
+    nome = input("Nome: ")
+    cpf = input("CPF: ")
+    tipo_conta = input("Tipo de conta (comum/plus): ")
+    saldo_inicial = float(input("Valor inicial da conta: "))
+    senha = input("Senha: ")
+
+    # Validação do tipo de conta
+    if tipo_conta not in ['comum', 'plus']:
+        print("Tipo de conta inválido!")
+        return
+
+    # Criação do cliente
+    clientes[cpf] = {
+        'nome': nome,
+        'tipo_conta': tipo_conta,
+        'saldo': saldo_inicial,
+        'senha': senha,
+        'extrato': [],
+    }
+
+    salvar_dados(clientes)
+    print("Cliente criado com sucesso!")
+
+# Função para apagar um cliente
+def apaga_cliente(clientes):
+    cpf = input("Informe o CPF do cliente a ser apagado: ")
+    if cpf in clientes:
+        del clientes[cpf]
+        salvar_dados(clientes)
+        print("Cliente apagado com sucesso!")
+    else:
+        print("Cliente não encontrado!")
